c17be8d8f23b3970c69aac6c43b08625fdd46c0d
Romulus <romulop.brasil25@gmail.com>
2025-02-18 21:35:06 -0300

    Imc

--- js/script.js
+++ js/script.js
@@ -77,6 +77,10 @@ function validDigits(text){
     return text.replace(/[^0-9,]/g, "");
 }
 
+function calcImc(weight,height) {
+    const imc = (weight / (height*height)).toFixed(1);
+    return imc;
+}
 // Inicialização
 createTable(data);
 // Eventos
@@ -89,6 +93,28 @@ createTable(data);
         e.target.value = updateValue;
     });
 });
+
+calcBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const weight = +weightInput.value.replace(",", ".");
+    const height = +heightInput.value.replace(",", ".");
+
+    if(!weight || !height ) return;
+
+    const imc = calcImc(weight,height);
+
+    let info;
+
+    data.forEach((item) =>{
+        if(imc >= item.min && imc <= item.max) {
+            info = item.info;
+        }
+    });
+
+    if(!info) return;
+});
+
 
 clearBtn.addEventListener("click", (e) => {
     e.preventDefault();
